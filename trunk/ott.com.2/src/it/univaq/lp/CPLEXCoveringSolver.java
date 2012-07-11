4e012831058f76b07e6105edc14df83cfdb167af
giacomolm@gmail.com <giacomolm@gmail.com@b51e6a44-9fa1-d70c-0c73-14ef886e3dd4>
2012-07-11 19:56:26 +0000

    Setting Cplex Environment

--- trunk/ott.com.2/src/it/univaq/lp/CPLEXCoveringSolver.java
+++ trunk/ott.com.2/src/it/univaq/lp/CPLEXCoveringSolver.java
@@ -1,15 +1,32 @@
 package it.univaq.lp;
 
+import ilog.concert.IloException;
+import ilog.cplex.IloCplex;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-public class CPLEXSolver implements CoveringSolver {
+import net.sf.javailp.Solver;
 
+public class CPLEXCoveringSolver implements CoveringSolver {
+
+	private Solver solver;
+	
+	public CPLEXCoveringSolver(Solver solver) {
+		this.solver = solver;
+	}
+	
 	@Override
 	public <E> double solve(Set<E> universe, Collection<Set<E>> subsets,
 			Set<E> solution) {
 		// TODO Auto-generated method stub
+		try {
+			IloCplex cplex = new IloCplex();
+		} catch (IloException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
